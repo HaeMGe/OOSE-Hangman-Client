2b118fec4dbe9060a51769c086ddec0a83a98043
Victor <Victor@LAPTOP-4918OAIF>
2022-06-14 21:41:51 +0200

    Dependencie aktualisiert für Okhttp und Beispiel postRequest

--- gradle/src/main/java/org/example/Main.java
+++ gradle/src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import okhttp3.OkHttpClient;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    static OkHttpClient client = new OkHttpClient();  //verwendeter Client
+    static PostClass posten = new PostClass();
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello world!");
+        String antwort = posten.doPostRequest("http://localhost:4567/games/hangman/start" , "Hallo vom Client!");
+        System.out.println(antwort);
+
     }
+
+
 }
---
+++ gradle/src/main/java/org/example/PostClass.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.awt.*;
+import java.io.IOException;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+
+/**
+ * Neue Ressourcen erschaffen und zu Server hinzufügen
+ */
+public class PostClass {
+
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+    static OkHttpClient client = new OkHttpClient();
+
+    String doPostRequest(String url, String json) throws IOException {
+        RequestBody body = RequestBody.create(JSON, json);   //neuen Request-Body mit json Inhalt
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        Response response = Main.client.newCall(request).execute();  //Response erhalten/Request losschicken
+        return response.body().string();
+    }
+}
