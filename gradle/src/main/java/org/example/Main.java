01a83137ac93723036fa75b95e6b8bcc102c48bd
Victor <Victor@LAPTOP-4918OAIF>
2022-06-15 20:42:45 +0200

    GetClass und einen Path ergänzt

--- gradle/src/main/java/org/example/Main.java
+++ gradle/src/main/java/org/example/Main.java
@@ -1,18 +1,36 @@
 package org.example;
-
 import okhttp3.OkHttpClient;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     static OkHttpClient client = new OkHttpClient();  //verwendeter Client
     static PostClass posten = new PostClass();
+    Scanner sc = new Scanner(System.in);
+    GetClass getter = new GetClass();
+
+    String wort;
     public static void main(String[] args) throws IOException {
         System.out.println("Hello world!");
         String antwort = posten.doPostRequest("http://localhost:4567/games/hangman/start" , "Hallo vom Client!");
         System.out.println(antwort);
 
     }
+
+
+  /**  public void setWort() throws IOException {
+        System.out.println("Ihr Wort:");
+        this.wort = sc.next();
+        String antwort = posten.doPostRequest("http://localhost:4567/games/hangman/start/neuesWort", "{ "+ wort + " }");
+
+    }
+   */
+  /**  public void getWort() throws IOException {
+        String raetsel = getter.run("http://localhost:4567/games/hangman/start/loesen");
+        System.out.println(raetsel.length());
+    }
+   */
 
 
 }
--- gradle/src/test/java/org/example/MainTest.java
+++ gradle/src/test/java/org/example/MainTest.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
+  @Test
+  void nebeneinander() {
+      Main prog1 = new Main();
+      Main prog2 = new Main();
+
+
+      prog1.setWort();
+  }
 
 }
