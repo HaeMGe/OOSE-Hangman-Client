15829735d4843cc49cbbe35a6e1bdc7e82ce7018
Victor <Victor@LAPTOP-4918OAIF>
2022-06-22 11:04:31 +0200

    neuen Pool anlegen

--- gradle/src/main/java/org/example/Ablauf.java
+++ gradle/src/main/java/org/example/Ablauf.java
@@ -7,9 +7,59 @@ public class Ablauf {
     static Scanner sc = new Scanner(System.in);
 
 
+public static void start() throws IOException {
+    System.out.println("Name: ");
+    String name = sc.next();
+    Main.name = name;
+
+    System.out.println("Was möchten Sie tuen?");
+    System.out.println("1: Spielpool beitreten");
+    System.out.println("2: Spielpool anlegen");
+    System.out.println("3. Logout");
+    int option = sc.nextInt();
+    menue1(option);
 
 
- public static void raten() throws IOException {
+}
+
+    private static void menue1(int option) throws IOException {
+    if(option == 1){
+        poolBeitreten();
+    }
+    if(option == 2){
+        poolAnlegen();
+    }
+    if(option == 3){
+        logout();
+    }
+    }
+
+    //logout
+    private static void logout() throws IOException {
+        start();
+    }
+
+    //neuen Pool anlegen
+    private static void poolAnlegen() throws IOException {
+        System.out.println("Welchen Schwierigkeitsgrad soll der Pool haben?");
+        int level = sc.nextInt();
+        System.out.println("Was soll die Pool-ID sein?");
+        int id = sc.nextInt();
+
+        String antwort = Main.posten.doPostRequest("http://localhost:4567/games/hangman/start/neuerPool/", "{ 'name': '"+ Main.name+ "','pool': '"+id+"','level': '"+level+"'}");  //neuen Postrequest mit Eingabe an Server
+        boolean antwort2 = Boolean.parseBoolean(antwort);
+
+        if(antwort2){
+            System.out.println("Ein Pool wurde erfolgreich angelegt.");
+        }
+        else System.out.println("Leider ist ein Fehler passiert. Probieren Sie eventuell eine andere ID aus.");
+
+
+    }
+
+    private static void poolBeitreten() {}
+
+    public static void raten() throws IOException {
      String eingabe = null;
      System.out.println("Bitte geben Sie die 0 ein, wenn Sie einen Buchstaben erraten möchten und eine 1, wenn Sie schon ein ganzes Wort probieren wollen.");
      int option = sc.nextInt();
@@ -22,8 +72,17 @@ public class Ablauf {
          eingabe = sc.next();
      }
 
-     String antwort = Main.posten.doPostRequest("http://localhost:4567/games/hangman/start/neuesWort/"+ option, "{ "+ eingabe + " }");  //neuen Postrequest mit Eingabe an Server
+     String antwort = Main.posten.doPostRequest("http://localhost:4567/games/hangman/start/neuesWort/"+ option, "{ 'name': '"+ Main.name+ "','pool': '"+Main.poolID+"','zeichen': '"+eingabe+"'}");  //neuen Postrequest mit Eingabe an Server
+     antwort.replace("{", "");
+     antwort.replace("}", "");
+     boolean antwort2 = Boolean.parseBoolean(antwort);
      System.out.println(antwort);
+     System.out.println(antwort2);
+     if(antwort2){
+         System.out.println("Richtig geraten!");
+     }
+     else System.out.println("Leider falscher Buchstabe! :-(");
+
 
  }
  }
--- gradle/src/main/java/org/example/Main.java
+++ gradle/src/main/java/org/example/Main.java
@@ -5,16 +5,22 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
+    static String name;
+    static int poolID;
     static OkHttpClient client = new OkHttpClient();  //verwendeter Client
     static PostClass posten = new PostClass();
     Scanner sc = new Scanner(System.in);
     GetClass getter = new GetClass();
+
 
     String wort;
     public static void main(String[] args) throws IOException {
         System.out.println("Hello world!");
         String antwort = posten.doPostRequest("http://localhost:4567/games/hangman/start" , "Hallo vom Client!");
         System.out.println(antwort);
+        Ablauf.start();
+
+
 
     }
 
