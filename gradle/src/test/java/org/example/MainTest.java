7ba19d8545d8aabdf74db6dd19a76ac1c9c055cc
Victor <Victor@LAPTOP-4918OAIF>
2022-06-23 13:20:08 +0200

    test entfernt

--- gradle/src/test/java/org/example/MainTest.java
+++ gradle/src/test/java/org/example/MainTest.java
@@ -5,13 +5,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
-  @Test
-  void nebeneinander() {
-      Main prog1 = new Main();
-      Main prog2 = new Main();
 
-
-      prog1.setWort();
-  }
 
 }
