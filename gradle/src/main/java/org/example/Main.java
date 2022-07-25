fc992383ad14df2c537bcf6dec303c7c04365a88
Tuhin <75586604+HaeMGe@users.noreply.github.com>
2022-07-25 21:54:13 +0200

    Update Main.java

--- gradle/src/main/java/org/example/Main.java
+++ gradle/src/main/java/org/example/Main.java
@@ -10,7 +10,7 @@ public class Main {
     static int poolID;   // Pool-ID von Pool in dem Nutzer gerade ist
     static OkHttpClient client = new OkHttpClient();  //verwendeter Client
     static PostClass posten = new PostClass(); //Object, um die Anfragen an den Server zu schicken
-    static String link = "http://localhost:4567/";  //Link, welcher Server benutzt werden soll
+    static String link = "http://tuhintest.ddns.net:5741/";  //Link, welcher Server benutzt werden soll
     //http://tuhintest.ddns.net:5741/
     //http://localhost:4567/
 
